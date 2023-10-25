70d353e2f11a0bec980b1922cbb6ed440838395f
Simon Baykov <simon.baykov@gmail.com>
2023-10-25 09:57:25 -0400

    added the decoded function

--- lab6.py
+++ lab6.py
@@ -1,3 +1,10 @@
 def main():
     x = 2
 
+
+def decode(pswd):
+    output = ''
+    for i in range(len(pswd)):
+        num = int(pswd[i]) - 3
+        output += str(num) if num >= 0 else str(10 + num)
+    return output
